00732c422dab1270adc9f178851e03601dde3674
Claudia Nou Carrera <claudianou@gmail.com>
2016-05-03 15:39:17 +0200

    Added all the files of the small project
    This project was an assignment for my school.
    We created a small math library and then we used Winforms and C# to create an interface for a calculator.

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Exercise12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClr = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnStressTest = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 35);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(82, 35);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(56, 19);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAddClick);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(82, 76);
+            this.btnSub.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(56, 19);
+            this.btnSub.TabIndex = 3;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSubClick);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(82, 118);
+            this.btnMul.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(56, 19);
+            this.btnMul.TabIndex = 4;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMulClick);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(82, 164);
+            this.btnDiv.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(56, 19);
+            this.btnDiv.TabIndex = 5;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDivClick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(263, 36);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "=";
+            // 
+            // btnClr
+            // 
+            this.btnClr.Location = new System.Drawing.Point(82, 207);
+            this.btnClr.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(56, 19);
+            this.btnClr.TabIndex = 5;
+            this.btnClr.Text = "Clr";
+            this.btnClr.UseVisualStyleBackColor = true;
+            this.btnClr.Click += new System.EventHandler(this.btnClrClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(263, 76);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(155, 160);
+            this.listBox1.TabIndex = 8;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(426, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OnFileOpen);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.OnFileSave);
+            // 
+            // btnStressTest
+            // 
+            this.btnStressTest.Location = new System.Drawing.Point(174, 164);
+            this.btnStressTest.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStressTest.Name = "btnStressTest";
+            this.btnStressTest.Size = new System.Drawing.Size(74, 27);
+            this.btnStressTest.TabIndex = 10;
+            this.btnStressTest.Text = "Stress Test";
+            this.btnStressTest.UseVisualStyleBackColor = true;
+            this.btnStressTest.Click += new System.EventHandler(this.OnStressTest);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 199);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Stress Test Async";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnStressTestAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 248);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStressTest);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnClr);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "DNPI1 - Exercise 12";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClr;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Button btnStressTest;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Exercise12
+{
+    public partial class Form1 : Form
+    {
+        private VIA.DNP1MathLib math;
+        bool threadRunning = true;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+            math = new VIA.DNP1MathLib();
+        }
+
+
+        bool bGetInput(ref int n1, ref int n2)
+        {
+            n1 = n2 = 0; 
+            try
+            {
+ 
+                n1 = int.Parse(this.textBox1.Text);
+                n2 = int.Parse(this.textBox2.Text);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("You entered something wrong!");
+            }
+            return false;
+        }
+        private void btnAddClick(object sender, EventArgs e)
+        {
+            int n1 = 0;
+            int n2 = 0;
+            if ( bGetInput( ref n1, ref n2 ) == true )
+            {
+                textBox3.Text = math.Add(n1, n2).ToString();
+                
+                string s = n1.ToString() + " + " + n2.ToString() + " = " + textBox3.Text;
+                listBox1.Items.Add(s);
+            }
+        }
+
+        private void btnSubClick(object sender, EventArgs e)
+        {
+            int n1 = 0;
+            int n2 = 0;
+            if (bGetInput(ref n1, ref n2) == true)
+            {
+                textBox3.Text = math.Sub(n1, n2).ToString();
+                string s = n1.ToString() + " - " + n2.ToString() + " = " + textBox3.Text;
+                listBox1.Items.Add(s);
+            }
+        }
+
+        private void btnMulClick(object sender, EventArgs e)
+        {
+            int n1 = 0;
+            int n2 = 0;
+            if (bGetInput(ref n1, ref n2) == true)
+            {
+                textBox3.Text = math.Mul(n1, n2).ToString();
+                string s = n1.ToString() + " * " + n2.ToString() + " = " + textBox3.Text;
+                listBox1.Items.Add(s);
+            }
+        }
+
+        private void btnDivClick(object sender, EventArgs e)
+        {
+            int n1 = 0;
+            int n2 = 0;
+            if (bGetInput(ref n1, ref n2) == true)
+            {
+
+                try
+                {
+                    textBox3.Text = math.Div(n1, n2).ToString();
+                    string s = n1.ToString() + " / " + n2.ToString() + " = " + textBox3.Text;
+                    listBox1.Items.Add(s);
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show( "UPS!");
+                }
+            }
+        }
+        private void btnClrClick(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            listBox1.Items.Clear();
+        }
+
+        private void OnFileOpen(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "XML Files|*.xml|All files (*.*)|*.*";
+            DialogResult dr = ofd.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                string filename = ofd.FileName;
+
+                using (XmlReader reader = XmlReader.Create(ofd.FileName))
+                {
+                    reader.ReadStartElement("data");
+
+                    while (reader.Name == "calculation")
+                    {
+                        XElement el = (XElement)XNode.ReadFrom(reader);
+                        listBox1.Items.Add(el.Value);
+                    }
+
+                    reader.ReadEndElement();
+                }
+            }
+        }
+
+        private void OnFileSave(object sender, EventArgs e)
+        {
+            SaveFileDialog ofd = new SaveFileDialog();
+            ofd.Filter = "XML Files|*.xml|All files (*.*)|*.*";
+            DialogResult dr = ofd.ShowDialog();
+
+            if ( dr == DialogResult.OK )
+            {
+               //string filename = ofd.FileName;
+               XmlWriter xmlWriter = XmlWriter.Create(ofd.FileName);
+     
+
+               xmlWriter.WriteStartDocument();
+               xmlWriter.WriteStartElement("data");
+
+               foreach (var item in listBox1.Items )
+               {
+                   xmlWriter.WriteStartElement("calculation");
+                   xmlWriter.WriteString( item.ToString() );
+                   xmlWriter.WriteEndElement();
+               }
+
+               xmlWriter.WriteEndDocument();
+               xmlWriter.Close();
+            }
+        }
+
+        private void OnStressTest(object sender, EventArgs e)
+        {
+            // ThreadEngine();
+            new Thread(ThreadEngine).Start();
+        }
+
+        void ThreadEngine()
+        {
+            while( threadRunning )
+            {
+                string total = "";
+                Random r = new Random();
+                int t1 = r.Next(100);
+                int t2 = r.Next(100);
+                int result = 0;
+                int whichMethod = r.Next(3);
+                switch (whichMethod)
+                {
+                    case 0:
+                        {
+                            result = math.Add(t1, t2);
+                            string s = t1.ToString() + " + " + t2.ToString() + " = " + result.ToString();
+                            total = s;
+                            break;
+                        }
+                    case 1:
+                        {
+                            result = math.Sub(t1, t2);
+                            string s = t1.ToString() + " - " + t2.ToString() + " = " + result.ToString();
+                            total = s;
+                            break;
+                        }
+                    case 2:
+                        {
+                            result = math.Mul(t1, t2);
+                            string s = t1.ToString() + " * " + t2.ToString() + " = " + result.ToString();
+                            total = s;
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (t2 > 0)
+                            {
+                                result = math.Div(t1, t2);
+                                string s = t1.ToString() + " / " + t2.ToString() + " = " + result.ToString();
+                                total = s;
+                            }
+                            break;
+                        }
+                }
+
+
+                if (this.InvokeRequired)
+                {
+                    MethodInvoker del = delegate { this.listBox1.Items.Add(total); };
+                    this.Invoke(del);
+                }
+                else
+                {
+                    this.listBox1.Items.Add(total);
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            threadRunning = false;
+        }
+
+
+        private async void OnStressTestAsync(object sender, EventArgs e)
+        {
+            int count = 0;
+            while ( count++ < 10 )
+            {
+                string total = "";
+                Random r = new Random();
+                int t1 = r.Next(100);
+                int t2 = r.Next(100);
+                int result = 0;
+                int whichMethod = r.Next(3);
+                switch (whichMethod)
+                {
+                    case 0:
+                        {
+                            result = math.Add(t1, t2);
+                            string s = t1.ToString() + " + " + t2.ToString() + " = " + result.ToString();
+                            total = s;
+                            break;
+                        }
+                    case 1:
+                        {
+                            result = math.Sub(t1, t2);
+                            string s = t1.ToString() + " - " + t2.ToString() + " = " + result.ToString();
+                            total = s;
+                            break;
+                        }
+                    case 2:
+                        {
+                            result = math.Mul(t1, t2);
+                            string s = t1.ToString() + " * " + t2.ToString() + " = " + result.ToString();
+                            total = s;
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (t2 > 0)
+                            {
+                                result = math.Div(t1, t2);
+                                string s = t1.ToString() + " / " + t2.ToString() + " = " + result.ToString();
+                                total = s;
+                            }
+                            break;
+                        }
+                }
+
+                MethodInvoker del = delegate { this.listBox1.Items.Add(total); };
+                await Task.Run(() => this.Invoke(del));
+
+                
+                Task delayTask = Task.Delay(1000);
+                await delayTask;
+            }
+        }
+    }
+}
